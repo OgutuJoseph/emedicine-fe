1a7bb872f38631f213b0c64357e411e7abc3baa7
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-18 15:39:48 +0300

    Configured routes

--- src/components/RouterPage.js
+++ src/components/RouterPage.js
@@ -1,11 +1,35 @@
 import  React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './Login';
+import Registration from './Registration'
+import Cart from './users/Cart';
+import Dashboard from './users/Dasboard';
+import Header from './users/Header';
+import MedicineDisplay from './users/MedicineDisplay';
+import Orders from './users/Orders';
+import Profile from './users/Profile';
+import AdminDasboard from './admin/AdminDashboard';
+import AdminHeader from './admin/AdminHeader';
+import AdminOrders from './admin/AdminOrders';
+import CustomerList from './admin/CustomerList';
+import Medicine from './admin/Medicine';
 
 export default function RouterPage(){
     return(
         <Router>
             <Routes>
-                {/* <Route path='/' element={}></Route> */}
+                <Route path='/' element={<Login />} />
+                <Route path='/registration' element={<Registration />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/products' element={<MedicineDisplay />} />
+                <Route path='/orders' element={<Orders />} />
+                <Route path='/profile' element={<Profile />} />
+                
+                <Route path='/admin/dashboard' element={<AdminDasboard />} />
+                <Route path='/admin/orders' element={<AdminOrders />} />
+                <Route path='/admin/customers' element={<CustomerList />} />
+                <Route path='admin/medicine' element={<Medicine />} />
             </Routes>
         </Router>
     )
